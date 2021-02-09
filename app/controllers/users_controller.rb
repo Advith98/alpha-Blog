7cb9a25df6909a61e6a4ef5380fb59f6d8273a04
Advith <advith.tm98@gmail.com>
2021-02-09 17:20:12 +0530

    modified layout and redirection link , minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Account has been updated Successfully"
-            redirect_to articles_path
+            redirect_to @user
         else  
             render 'edit'
         end
